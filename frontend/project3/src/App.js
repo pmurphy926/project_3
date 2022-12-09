3d43c244a4cb11221e37dfa4e636166bee6904f7
pmurphy926 <patrick.murphy926@gmail.com>
2022-12-09 11:25:06 -0500

    front end set started

---
+++ frontend/project3/src/App.js
@@ -0,0 +1,13 @@
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Project 3</h1>
+    </div>
+  );
+}
+
+export default App;
